e2f4ff48ff4166a888bda3d9e779e12dd60ddd2f
Alex Gottschalk <alex.gottschalk@gmail.com>
2018-07-08 15:19:46 -0700

    unhide files

--- install.sh
+++ install.sh
@@ -5,24 +5,6 @@
 
 #!/bin/bash
 
-trap exit ERR
-if [ -d $HOME/.tmux  ]
-then
-    echo .tmux already exist
-    mv $HOME/.tmux $HOME/.tmux.bak
-fi
+git submodule init && git submodule update
 
-if [ -e $HOME/.tmux.conf  ]
-then
-    echo .tmux.conf already exist
-    mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
-fi
-
-cp -r $HOME/tmux-config $HOME/.tmux
-ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
-
-cd ~/.tmux && git submodule init && git submodule update
-
-cd ~/.tmux/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
-
-tmux source-file ~/.tmux.conf
+cd vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
